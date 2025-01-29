f30dea11fbbc1dbccb070781fd7536a00757457b
KlaraMat <168363447+KlaraMat@users.noreply.github.com>
2025-01-29 08:37:44 +0100

    Add files via upload
    vjezbe iz polugodista

---
+++ pzw/skripta-3.js
@@ -0,0 +1,18 @@
+// JS DOM
+
+function provjeri() {
+
+    let ime = document.forms[0].elements[0].value;
+
+    let prezime = document.forms[0].elements[1].value;
+    
+    let razred = document.getElementById("razred").value;
+    
+    let skola = document.forms[0].elements[3].value;
+
+    let grad = document.getElementById("Grad").value;
+    
+    
+    alert(ime + " " + prezime + " " + razred + " " + skola + " " + grad);
+
+}
